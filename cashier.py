8e27089bc201b8383492a9fda411dcf988320333
Dhio Ackmal Farezi Putra <136438549+DhioAckmal@users.noreply.github.com>
2023-07-09 17:35:44 +0700

    Added files

---
+++ cashier.py
@@ -0,0 +1,300 @@
+import tabulate
+
+shopping_list = []
+
+#Add Item
+def add_item():
+    """
+    Fungsi add_item
+
+    Tujuan: input dan menambahkan barang ke list shopping_list
+    """
+    
+    while True:
+        # Menginput barang yang ingin dibeli
+        while True:
+            name = input("Masukkan nama barang (ketik 'end' untuk keluar): ").lower()
+            if name.isnumeric():
+                print("---Hanya masukkan nama barang.---")
+            elif name == 'end':
+                return  # Keluar dari fungsi jika input 'end'
+            else:
+                break
+        
+        # Menginput jumlah barang yang ingin dibeli
+        while True:
+            try:
+                qty = int(input("Jumlah barang yang ingin dibeli: "))
+                break
+            except ValueError:
+                print("---Hanya masukkan angka untuk jumlah barang.---")
+
+        # Menginput harga barang yang ingin dibeli
+        while True:
+            try:
+                price = float(input("Masukkan harga barang: "))
+                break
+            except ValueError:
+                print("---Hanya masukkan angka untuk harga.---")
+        
+        # Total harga barang
+        total_price = qty * price
+                
+        # Menambahkan nomor barang
+        item_id = len(shopping_list) + 1
+    
+        # Membuat dictionary untuk ditambahkan ke list shopping_list
+        shopping_list.append({
+            'No': item_id,
+            'Nama': name,
+            'Jumlah': qty,
+            'Harga': price,
+            'Total Harga': total_price
+        })
+    
+        print(f'============================')
+        print(f"Barang berhasil ditambahkan")
+        
+
+#Update Item
+def update_item():
+    """
+    Fungsi update_item()
+    
+    Tujuan: merubah nama barang
+    """
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+    while True:
+        try:
+            item_id = int(input("Masukan nomor item yang ingin diubah: "))
+            break
+        except ValueError:
+            print("---Nomor item salah---")
+    new_name = input("Masukan nama item baru: ")
+
+    for item in shopping_list:
+        if item['No'] == item_id:
+            item['Nama']= new_name
+            break
+
+    print("=" * 60)
+    print("---Item berhasil diubah---")
+    print("=" * 60)
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+
+#Update Price
+def update_price():
+    """
+    Fungsi update_price()
+    
+    Tujuan: merubah harga barang
+    """
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+    while True:
+        try:
+            item_id = int(input("Masukan nomor item yang ingin diubah: "))
+            break
+        except ValueError:
+            print("---Nomor item salah---")
+    new_price = float(input("Masukan harga item baru "))
+
+    for item in shopping_list:
+        if item['No'] == item_id:
+            item['Harga'] = new_price
+            break
+
+    print("=" * 60)
+    print("---Item berhasil diubah---")
+    print("=" * 60)
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+
+#Update Quantity
+def update_qty():
+    """
+    Fungsi update_qty()
+    
+    Tujuan: merubah jumlah barang
+    """
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+    while True:
+        try:
+            item_id = int(input("Masukan nomor item yang ingin diubah: "))
+            break
+        except ValueError:
+            print("---Nomor item salah---")
+    new_qty = int(input("Masukan jumlah item baru: "))
+
+    for item in shopping_list:
+        if item['No'] == item_id:
+            item['Jumlah'] = new_qty
+            break
+        
+    print("=" * 60)
+    print("---Item berhasil diubah---")
+    print("=" * 60)
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+ 
+#Delete Item
+def delete_item():
+    """
+    Fungsi delete_item
+
+    Tujuan: menghapus list belanjaan yang dipilih.
+    
+    Cara: Akan menampilkan tabel dan user akan memilih nomor item untuk di delete,
+    terakhir akan menampilkan shopping_list yang sudah di update.
+    """
+    # menampilkan nomor item untuk di delete
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+    item_id = int(input("Masukan nomor item yang ingin dihapus: "))
+
+    # menggunakan looping untuk mencari no item yang sesuai
+    for i, item in enumerate(shopping_list):
+        if item['No'] == item_id:
+            shopping_list.remove(item)
+    
+            # mengupdate item number yang baru
+            for j in range(i, len(shopping_list)):
+                shopping_list[j]['no'] -= 1
+            break
+
+    # print pesan bahwa telah berhasil di update
+    print("=" * 60)
+    print("---Barang berhasil dihapus.---")
+    print("=" * 60)
+    
+    print(tabulate.tabulate(shopping_list, headers='keys'))
+    print("\n")
+
+#Show Items 
+def show_items():
+    """
+    Fungsi show_items
+
+    Tujuan: menampilkan list shopping_list dengan format tabel menggunakan
+    library tabulate.
+    """
+    
+    if not shopping_list:
+        print("=" * 60)
+        print("---Tidak ada barang, silahkan tambahkan barang.---")
+        print("=" * 60)
+    else:
+        # menggunakan library tabulate untuk menampilkan list shopping_list
+        print(tabulate.tabulate(shopping_list, headers='keys', tablefmt='fancy_grid'))
+   
+#Reset Transaction
+def reset_transaction():
+    """
+    Fungsi reset_transaction
+    
+    Tujuan: menghapus atau reset semua list di shopping_list
+    """
+    confirm = input("Apakah anda yakin menghapus list belanjaan anda? (y/n)").lower()
+
+    if confirm == 'y':
+        shopping_list.clear()
+        print("=" * 60)
+        print("---List belanjaan berhasil dihapus.---")
+    else:
+        print("=" * 60)
+        print("---Penghapusan list belanjaan dibatalkan.---")
+
+# Total Price  
+def total_price():
+    """
+    Fungsi total_price
+    
+    Tujuan: Menghitung total harga list belanja, menghitung diskon yang didapatkan, 
+    berdasarkan harga yang dibelanjakan, dan menghitung total harga setelah diskon.
+
+    """
+    total_price = 0
+    for item in shopping_list:
+        total_price += item['Harga'] * item['Jumlah']
+    
+    discount = 0
+
+    # menentukan diskon yang didapat berdasarkan requirement
+    if total_price > 500_000:
+        discount = 0.1
+    elif total_price > 300_000:
+        discount = 0.08
+    elif total_price > 200_000:
+        discount = 0.05
+
+    print(tabulate.tabulate(shopping_list, headers='keys', tablefmt='fancy_grid'))
+
+    discount_amount = total_price * discount # menghitung potongan harga
+    discounted_price = total_price - discount_amount # menghitung harga setelah diskon
+    
+    # menampilkan total harga, diskon, dan harga setelah diskon
+    print("=" * 60)
+    print(f"Harga: Rp. {total_price}")
+    print(f"Diskon: {discount * 100}%")
+    print(f"Total Diskon: Rp. {discount_amount}")
+    print(f"Total Harga: Rp. {discounted_price}")
+    print("=" * 60)
+ 
+#Checkout (payment) 
+def checkout():
+    """
+    Fungsi checkout()
+    
+    Tujuan: menampilkan total harga belanja setelah diskon dan tempat untuk melakukan
+    pembayaran.
+
+    """
+    total_price = 0
+    for item in shopping_list:
+        total_price += item['Harga'] * item['Jumlah']
+    
+    discount = 0
+
+    # menentukan diskon yang didapat berdasarkan requirement
+    if total_price > 500_000:
+        discount = 0.1
+    elif total_price > 300_000:
+        discount = 0.08
+    elif total_price > 200_000:
+        discount = 0.05
+
+    print("Super Cashier")
+    print("instagram : Super Cashier")
+    print(tabulate.tabulate(shopping_list, headers='keys', tablefmt='fancy_grid'))
+
+    discount_amount = total_price * discount # menghitung potongan harga
+    discounted_price = total_price - discount_amount # menghitung harga setelah diskon
+    
+    # menampilkan total harga, diskon, besaran diskon, dan harga setelah diskon
+    print("=" * 60)
+    print(f"Harga: Rp. {total_price}")
+    print(f"Diskon: {discount * 100}%")
+    print(f"Total Diskon: Rp. {discount_amount}")
+    print(f"Total Harga: Rp. {discounted_price}")
+    print("=" * 60)
+    
+    
+    while True:
+        try:
+            payment = float(input("Masukan nominal pembayaran: "))
+        except ValueError: 
+            print("=" * 60)
+            print("---Input salah. Hanya masukkan angka.---")
+            print("=" * 60)
+            continue
+        
+        if payment < discounted_price:
+            print("=" * 60)
+            print("---Nominal kurang---")
+            print("=" * 60)
+        else:
+            break
+    
+    # menghitung kembalian
+    change = payment - discounted_price
+    print(f"Kembali: Rp. {change:.1f}")
+    print("=" * 60)
+    print("Terima kasih telah berbelanja")
+
+    
---
+++ main.py
@@ -0,0 +1,197 @@
+from cashier import *
+import tabulate
+import random
+from datetime import datetime
+import sys
+"""
+Self Service Cashier.
+
+Tujuan: Merupakan program untuk membantu customer supermarket dalam menambah barang, merubah barang, menghapus barang,
+menampilkan barang, sampai ke pembayaran.
+
+"""
+
+print("=" * 60)
+print(f"Selamat Datang di Supermarket")
+print(f"Self Service Cashier System")
+print("=" * 60)
+
+# Input nama customer
+while True:
+    name = input("Masukan Nama: ")
+    if name.title():
+        break
+    else:
+        print("Input salah. Please enter alphabetic characters only.")
+
+# Membuat unique transaction id untuk setiap customer
+transaction_id = "TX" + str(random.randint(100, 999))
+
+# Membuat waktu dan tanggal transaksi
+current_date = datetime.now()
+date = current_date.strftime("%d/%m/%Y %H:%M:%S")
+
+# Menampilkan profil customer
+print(f"Name    : {name.title()}")
+print(f"Date    : {date}")
+print(f"TX ID   : {transaction_id}")
+print("=" * 60)
+
+# Menambahkan item ke list
+print(" Tambah Barang ")
+while True:
+    add_item()
+    print("=" * 60)
+    more_items = input("Anda yakin ingin keluar? (y/n): ").lower()
+    if more_items == "y":
+        break
+
+# Menampilkan menu super cashier
+while True:
+    print("=" * 60)
+    print("Menu Utama")
+    print("1. Tambah barang")
+    print("2. Update barang")
+    print("3. Hapus barang")
+    print("4. Tampilkan semua barang")
+    print("5. Reset barang")
+    print("6. Total harga")
+    print("7. Checkout")
+    print("8. Keluar")
+
+    try:
+        print("=" * 60)
+        choice = int(input("Pilih angka: "))
+    except ValueError:
+        print("Input salah, hanya masukkan angka")
+        continue
+
+    print("=" * 60)
+    if choice == 1:
+        print("---TAMBAH BARANG")
+        while True:
+            add_item()
+            more_items = input("Tambah barang? (y/n): ").lower()
+            print("\n")
+            if more_items == "n":
+                break
+                
+    elif choice == 2:
+        print("---UPDATE BARANG")
+        update_menu = True
+        while update_menu:
+            print("1. Update nama item")
+            print("2. Update harga item")
+            print("3. Update jumlah item")
+            print("4. Kembali ke menu utama")
+
+            try:
+                update_choice = int(input("Pilih angka: "))
+            except ValueError:
+                print("Input salah, hanya masukkan angka")
+                continue
+
+            if update_choice == 1:
+                update_item()
+
+            elif update_choice == 2:
+                update_price()
+
+            elif update_choice == 3:
+                update_qty()
+
+            elif update_choice == 4:
+                update_menu = False
+
+            else:
+                print("Input Salah")
+
+    elif choice == 3:
+        print("---HAPUS ITEM")
+        delete_menu = True
+        while delete_menu:
+            print("1. Hapus item")
+            print("2. Kembali ke menu utama")
+
+            try:
+                delete_choice = int(input("Pilih angka:"))
+            except ValueError:
+                print("Input salah, hanya masukkan angka")
+                continue
+
+            # Execute user's update choice
+            if delete_choice == 1:
+                delete_item()
+
+            elif delete_choice == 2:
+                delete_menu = False
+
+            else:
+                print("Input salah")
+
+    elif choice == 4:
+        print("---TAMPILKAN SEMUA BARANG")
+        show_items()
+
+    elif choice == 5:
+        print("---RESET BARANG")
+        reset_transaction()
+
+    elif choice == 6:
+        print("---MENGHITUNG TOTAL HARGA")
+        total_price()
+
+    elif choice == 7:
+        print("---CHECKOUT BARANG")
+        checkout_menu = True
+        while checkout_menu:
+            print("   1. Ya")
+            print("   2. Tidak")
+
+            try:
+                checkout_choice = int(input("Apakah anda yakin ingin checkout? "))
+            except ValueError:
+                print("Input salah, hanya masukkan angka")
+                continue
+
+            # Execute user's checkout choice
+            if checkout_choice == 1:
+                checkout()
+                print("============= Terima kasih telah belanja! ==============")
+                print("=" * 60)
+                reset_transaction()
+                checkout_menu = False
+
+            elif checkout_choice == 2:
+                checkout_menu = False
+
+            else:
+                print("Input salah")
+
+    elif choice == 8:
+        exit_menu = True
+        while exit_menu:
+            print("Keluar")
+            print("1. Ya")
+            print("2. Tidak")
+
+            try:
+                exit_choice = int(input("Apakah yakin ingin keluar? "))
+            except ValueError:
+                print("Input salah, hanya masukkan angka")
+                continue
+
+            if exit_choice == 1:
+                print("=" * 60)
+                print("============= Terima kasih telah berbelanja! ==============")
+                print("=" * 60)
+                sys.exit()
+
+            elif exit_choice == 2:
+                exit_menu = False
+
+            else:
+                print("Input salah")
+
+    else:
+        print("Input salah")
